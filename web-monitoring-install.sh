170d663ec744565df63ef2d3a4f85fe4f48af8d2
zys8119 <770959294@qq.com>
2022-11-16 11:45:19 +0800

    [add] 安装程序

--- web-monitoring-install.sh
+++ web-monitoring-install.sh
@@ -6,6 +6,8 @@ ln -s /node-v19.1.0-linux-arm64/bin/node /usr/local/bin/node
 ln -s /node-v19.1.0-linux-arm64/bin/npm /usr/local/bin/npm
 npm config set registry https://registry.npmmirror.com/
 npm i pm2 -g
+ln -s /node-v19.1.0-linux-arm64/bin/pm2 /usr/local/bin/pm2
 apt install nginx -y
+nginx
 echo "服务挂起"
 tail -f /dev/null
