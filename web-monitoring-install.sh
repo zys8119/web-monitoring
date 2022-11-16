8abea668d02eae34f0442f9d402404e515df426e
zys8119 <770959294@qq.com>
2022-11-16 11:33:29 +0800

    [add] 安装程序

--- web-monitoring-install.sh
+++ web-monitoring-install.sh
@@ -1,7 +1,6 @@
 apt update
 apt install wget -y
 apt install vim -y
-wget https://nodejs.org/dist/v19.1.0/node-v19.1.0-linux-arm64.tar.gz
 tar -xvf node-v19.1.0-linux-arm64.tar.gz
 ln -s /node-v19.1.0-linux-arm64/bin/node /usr/local/bin/node
 ln -s /node-v19.1.0-linux-arm64/bin/npm /usr/local/bin/npm
