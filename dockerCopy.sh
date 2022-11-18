9d1a4d2fb0ebc514da0c0bb0a9fdf362c2cfc97e
zys8119 <770959294@qq.com>
2022-11-18 22:24:09 +0800

    [add] dockerCopy.sh

--- dockerCopy.sh
+++
@@ -1,5 +0,0 @@
-docker cp ./node web-monitoring:node
-docker cp ./web web-monitoring:web
-docker cp ./node-v19.1.0-linux-arm64.tar.gz web-monitoring:node-v19.1.0-linux-arm64.tar.gz
-docker cp ./web-monitoring-install.sh web-monitoring:web-monitoring-install.sh
-docker cp ./docker-compose.yml web-monitoring:docker-compose.yml
