7078d1aef37400ae988e1a7219b3ac99048057eb
zys8119 <770959294@qq.com>
2022-11-30 15:16:02 +0800

    [add] 安装程序

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 docker cp ./ubuntu-install.sh ubuntu:/ubuntu-install.sh
-docker cp ./nginx/sites-enabled/web.conf ubuntu:/etc/nginx/sites-enabled/web.conf
+docker cp ./nginx/sites-enabled/web.conf ubuntu:/web.conf
